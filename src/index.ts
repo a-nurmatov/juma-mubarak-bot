e7d94d0f8f95cf051801757165470be9f7f347e4
anurmatov <anurmatov@exadel.com>
2025-03-02 16:07:03 +0500

    respond to all groups

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Telegraf, Context } from 'telegraf';
+import { Telegraf } from 'telegraf';
 import * as cron from 'node-cron';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
@@ -11,8 +11,8 @@ const GROUP_CHAT_ID: string = process.env.GROUP_CHAT_ID?.trim() || '';
 const UNSPLASH_CLIENT_ID: string = process.env.UNSPLASH_CLIENT_ID?.trim() || '';
 
 if (!BOT_TOKEN || !GROUP_CHAT_ID) {
-    console.error('❌ BOT_TOKEN and GROUP_CHAT_ID must be set.');
-    process.exit(1);
+  console.error('❌ BOT_TOKEN and GROUP_CHAT_ID must be set.');
+  process.exit(1);
 }
 
 // Initialize bot
@@ -20,67 +20,73 @@ const bot = new Telegraf(BOT_TOKEN);
 
 // Logger function to avoid repetition
 function log(type: 'info' | 'error', message: string, error?: any): void {
-    const prefix = type === 'info' ? 'ℹ️' : '❌';
-    if (type === 'info') {
-        console.log(`${prefix} ${message}`);
-    } else {
-        console.error(`${prefix} ${message}`, error instanceof Error ? error.message : error);
-    }
+  const prefix = type === 'info' ? 'ℹ️' : '❌';
+  if (type === 'info') {
+    console.log(`${prefix} ${message}`);
+  } else {
+    console.error(
+      `${prefix} ${message}`,
+      error instanceof Error ? error.message : error
+    );
+  }
 }
 
 // Function to fetch a random mosque image
 async function getJumaImage(): Promise<string> {
-    if (UNSPLASH_CLIENT_ID) {
-        try {
-            const response = await axios.get('https://api.unsplash.com/photos/random', {
-                params: { query: 'mosque', client_id: UNSPLASH_CLIENT_ID },
-            });
-            return response.data.urls.regular;
-        } catch (error) {
-            log('error', 'Unsplash API error', error);
+  if (UNSPLASH_CLIENT_ID) {
+    try {
+      const response = await axios.get(
+        'https://api.unsplash.com/photos/random',
+        {
+          params: { query: 'mosque', client_id: UNSPLASH_CLIENT_ID },
         }
+      );
+      return response.data.urls.regular;
+    } catch (error) {
+      log('error', 'Unsplash API error', error);
     }
-    return `https://images.unsplash.com/photo-1592326871020-04f58c1a52f3?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
+  }
+  return `https://images.unsplash.com/photo-1592326871020-04f58c1a52f3?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
 }
 
 // Function to generate a Juma Mubarak caption
 function generateCaption(senderName?: string): string {
-    let caption = `🌙 *Juma Muborak!*  
-🕌 Allohning rahmati, muhabbati va barakasi ustingizga yog‘ilsin.  
-📿 Ushbu muborak kun duolar, istaklar va ezgu niyatlar qabul bo‘ladigan fursat bo‘lsin.  
-
+  let caption = `🌙 *Juma Muborak!* \n
 🤲 *Alhamdulillah!* Bizni yana bir juma kuniga yetkazgan Allohga hamd bo‘lsin.  
 🕋 *La ilaha illallah!* Uning rahmati cheksiz, marhamati bitmas-tuganmas.  
 📖 *"Bas, Meni yod eting, Men ham sizni yod etaman!"* *(Baqara: 152)*  
-🕌 *Allohning salomi, rahmati va barakoti Payg‘ambarimiz Muhammad Mustafo ﷺ ga bo‘lsin!*  
+🕌 *Allohning salomi, rahmati va barakoti Payg‘ambarimiz Muhammad Mustafo ﷺ ga bo‘lsin!* 
 
-✨ Bugungi juma sizga ezgulik, qalbingizga osoyishtalik, oilangizga tinchlik va hayotingizga fayz olib kelsin.  
-📿 *Alloh sizni va yaqinlaringizni doimo himoya qilsin!*  
-🤲 *Duolaringiz ijobat bo‘lsin!*`;
+🕌 Allohning rahmati, muhabbati va barakasi ustingizga yog‘ilsin.  
+📿 Ushbu muborak kun duolar, istaklar va ezgu niyatlar qabul bo‘ladigan fursat bo‘lsin.
+`;
 
-    if (senderName) {
-        caption += `\n\n🖋 *${senderName}*`;
-    }
+  if (senderName) {
+    caption += `\n\n🖋 *${senderName}*`;
+  }
 
-    return caption;
+  return caption;
 }
 
 // Function to send Juma Mubarak image
 async function sendJumaMubarak(senderName?: string): Promise<void> {
-    try {
-        const imageUrl = await getJumaImage();
-        const caption = generateCaption(senderName);
+  try {
+    const imageUrl = await getJumaImage();
+    const caption = generateCaption(senderName);
 
-        await bot.telegram.sendPhoto(GROUP_CHAT_ID, imageUrl, { caption, parse_mode: 'Markdown' });
+    await bot.telegram.sendPhoto(GROUP_CHAT_ID, imageUrl, {
+      caption,
+      parse_mode: 'Markdown',
+    });
 
-        log('info', 'Juma Mubarak message sent!');
-    } catch (error) {
-        log('error', 'Error sending message', error);
-    }
+    log('info', 'Juma Mubarak message sent!');
+  } catch (error) {
+    log('error', 'Error sending message', error);
+  }
 }
 
 bot.command('yordam', async (ctx) => {
-    const helpMessage = `
+  const helpMessage = `
 🤖 *Bot Buyruqlari*:
 
 /jumaTabrik - Juma muborak rasmini yuborish  
@@ -89,56 +95,55 @@ bot.command('yordam', async (ctx) => {
 ℹ️ Buyruqlardan foydalanish uchun *"/"* belgisini kiriting va kerakli buyruqni tanlang.
     `;
 
-    await ctx.reply(helpMessage, { parse_mode: 'Markdown' });
+  await ctx.reply(helpMessage, { parse_mode: 'Markdown' });
 });
 
 // Handle /jumaMubarak command in group chat
-bot.command('jumaTabrik', async (ctx) => {
-    if (ctx.chat.id.toString().trim() === GROUP_CHAT_ID) {
-        const senderName = ctx.from?.first_name
-            ? `${ctx.from.first_name}${ctx.from.last_name ? ' ' + ctx.from.last_name : ''}`
-            : `Muallif noma'lum`; // Fallback if no name available
+bot.command('tabrik', async (ctx) => {
+  const senderName = ctx.from?.first_name
+    ? `${ctx.from.first_name}${ctx.from.last_name ? ' ' + ctx.from.last_name : ''}`
+    : `Muallif noma'lum`; // Fallback if no name available
 
-        await sendJumaMubarak(senderName);
-    } else {
-        await ctx.reply('This command is only available in the designated group.');
-    }
+  await sendJumaMubarak(senderName);
 });
-
 
 // Schedule cron job for every Friday at 5:00 AM (UTC) - No sender name for automated messages
 cron.schedule('0 5 * * 5', async () => {
-    log('info', 'Running scheduled Juma Mubarak task at 5:00 AM (UTC)...');
-    try {
-        await sendJumaMubarak(); // No sender name in automated messages
-        log('info', 'Juma Mubarak message successfully sent.');
-    } catch (error) {
-        log('error', 'Scheduled task error', error);
-    }
+  log('info', 'Running scheduled Juma Mubarak task at 5:00 AM (UTC)...');
+  try {
+    await sendJumaMubarak(); // No sender name in automated messages
+    log('info', 'Juma Mubarak message successfully sent.');
+  } catch (error) {
+    log('error', 'Scheduled task error', error);
+  }
 });
 
 // Launch bot
 (async () => {
-    try {
-        await bot.launch(() => {
-            log('info', 'Bot is running...')
-        });
-    } catch (error) {
-        log('error', 'Failed to start bot', error);
-        process.exit(1);
-    }
+  try {
+    await bot.launch(() => {
+      log('info', 'Bot is running...');
+    });
+  } catch (error) {
+    log('error', 'Failed to start bot', error);
+    process.exit(1);
+  }
 })();
 
 // Graceful shutdown
 process.once('SIGINT', () => {
-    log('info', 'Shutting down bot...');
-    bot.stop('SIGINT');
+  log('info', 'Shutting down bot...');
+  bot.stop('SIGINT');
 });
 process.once('SIGTERM', () => {
-    log('info', 'Shutting down bot...');
-    bot.stop('SIGTERM');
+  log('info', 'Shutting down bot...');
+  bot.stop('SIGTERM');
 });
 
 // Handle unexpected errors
-process.on('uncaughtException', (error) => log('error', 'Uncaught Exception', error));
-process.on('unhandledRejection', (reason) => log('error', 'Unhandled Promise Rejection', reason));
+process.on('uncaughtException', (error) =>
+  log('error', 'Uncaught Exception', error)
+);
+process.on('unhandledRejection', (reason) =>
+  log('error', 'Unhandled Promise Rejection', reason)
+);
